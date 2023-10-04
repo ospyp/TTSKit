585aa106a9cfcc4d7b57f9d0e92900bde024f158
mrfakename <me@mrfake.name>
2023-10-04 12:56:30 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyttskit',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     author='mrfakename',
     author_email='me@mrfake.name',
